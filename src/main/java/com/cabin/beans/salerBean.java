b06fd01b36981d86d4d4a3f78dadb4ca305b7c6a
Paul Luchyn <Paul Luchyn>
2016-06-28 15:36:55 +0300

    refactor

--- src/main/java/com/cabin/beans/salerBean.java
+++ src/main/java/com/cabin/beans/salerBean.java
@@ -1,12 +1,39 @@
 package com.cabin.beans;
 
+import com.cabin.entity.Saler;
+import com.cabin.entity.UpworkBid;
+import com.cabin.enums.Role;
 import lombok.Getter;
 import lombok.Setter;
 
-/**
- * Created by Ультрамар on 22.06.2016.
- */
+import java.util.List;
+
 @Getter
 @Setter
 public class SalerBean {
+
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String login;
+    private String password;
+    private String email;
+    private String phonenumber;
+    private Role role;
+    private List<UpworkBid> upworkBidList;
+
+    public static SalerBean toBean(Saler saler) {
+        SalerBean bean = new SalerBean();
+        bean.setId(saler.getId());
+        bean.setFirstname(saler.getFirstname());
+        bean.setLastname(saler.getLastname());
+        bean.setLogin(saler.getLogin());
+        bean.setPassword(saler.getPassword());
+        bean.setEmail(saler.getEmail());
+        bean.setPhonenumber(saler.getPhonenumber());
+        bean.setRole(saler.getRole());
+        bean.setUpworkBidList(saler.getUpworkBidList());
+        return bean;
+    }
+
 }
