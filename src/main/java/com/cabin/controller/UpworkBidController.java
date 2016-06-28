68e5c7ba72e97feb82269c3f95d89040863c3c15
Paul Luchyn <Paul Luchyn>
2016-06-28 17:09:43 +0300

    refactor

--- src/main/java/com/cabin/controller/UpworkBidController.java
+++ src/main/java/com/cabin/controller/UpworkBidController.java
@@ -5,10 +5,7 @@ import com.cabin.entity.UpworkBid;
 import com.cabin.service.interfaces.UpworkBidService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
@@ -21,59 +18,59 @@ public class UpworkBidController {
     @Autowired
     private UpworkBidService upworkBidService;
 
-    @RequestMapping("/add")
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
     public void add(@ModelAttribute UpworkBid bid) {
         upworkBidService.add(bid);
     }
 
-    @RequestMapping("/update")
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
     public void update(@ModelAttribute UpworkBid bid) {
         upworkBidService.update(bid);
     }
 
-    @RequestMapping("/delete")
+    @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
     public void delte(@RequestParam(value = "id") Integer id) {
         upworkBidService.delete(id);
     }
 
-    @RequestMapping("/getBySaler")
+    @RequestMapping(value = "/getBySaler", method = RequestMethod.GET)
     public List<UpworkBidBean> getBidsBySaler(@RequestParam(value = "salerId") Integer salerId) {
         return upworkBidService.getBySaler(salerId);
     }
 
-    @RequestMapping("/getByComment")
+    @RequestMapping(value = "/getByComment", method = RequestMethod.GET)
     public List<UpworkBidBean> getBidsBySaler(@RequestParam(value = "comment") String comment) {
         return upworkBidService.getByComment(comment);
     }
 
-    @RequestMapping("/getByDate")
+    @RequestMapping(value = "/getByDate", method = RequestMethod.GET)
     public List<UpworkBidBean> getBidsByDate(@RequestParam(value = "date") Date date) {
         return upworkBidService.getByDate(date);
     }
 
-    @RequestMapping("/getByUpdateDate")
+    @RequestMapping(value = "/getByUpdateDate", method = RequestMethod.GET)
     public List<UpworkBidBean> getBidsByUpdateDate(@RequestParam(value = "date") Date date) {
         return upworkBidService.getByUpdatedDate(date);
     }
 
-    @RequestMapping("/getByUrl")
+    @RequestMapping(value = "/getByUrl", method = RequestMethod.GET)
     public UpworkBidBean getBidsByUrl(@RequestParam(value = "url") String url) {
         return upworkBidService.getByUrl(url);
     }
 
-    @RequestMapping("/getBySalerAndDateBetween")
+    @RequestMapping(value = "/getBySalerAndDateBetween", method = RequestMethod.GET)
     public List<UpworkBidBean> getBidsBySalerAndDateBetween(@RequestParam(value = "salerId") Integer salerId,
             @RequestParam(value = "dateFrom") Date dateFrom, @RequestParam(value = "dateTo") Date dateTo) {
         return upworkBidService.getBySalerAndDateBetween(salerId, dateFrom, dateTo);
     }
 
-    @RequestMapping("/getBySalerAndUpdatedDateBetween")
+    @RequestMapping(value = "/getBySalerAndUpdatedDateBetween", method = RequestMethod.GET)
     public List<UpworkBidBean> getBidsBySalerAndUpdatedDateBetween(@RequestParam(value = "salerId") Integer salerId,
             @RequestParam(value = "dateFrom") Date dateFrom, @RequestParam(value = "dateTo") Date dateTo) {
         return upworkBidService.getBySalerAndUpdatedDateBetween(salerId, dateFrom, dateTo);
     }
 
-    @RequestMapping("/getAllBetweenDate")
+    @RequestMapping(value = "/getAllBetweenDate", method = RequestMethod.GET)
     public List<UpworkBidBean> getAllBetweenDate(
             @RequestParam(value = "dateFrom") Date dateFrom, @RequestParam(value = "dateTo") Date dateTo) {
         return upworkBidService.getAllBetweenDate(dateFrom, dateTo);
